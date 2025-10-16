c544663a438b6ae29b0811a1b486a708aa92d73c
22f3003001 <22f3003001@ds.study.iitm.ac.in>
2025-10-16 16:59:44 +0530

    Update build.js

--- api/build.js
+++ api/build.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  return res.status(200).json({ success: true, message: "API is working" });
-}
+module.exports = (req, res) => {
+  res.status(200).json({ success: true, message: "API is working" });
+};
